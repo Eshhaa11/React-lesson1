10356a35f5183e643a1937a227282219486f886e
Esha Patel <Patellesha11@gmail.com>
2025-03-12 17:12:33 +0300

    Add LogInComponent to App for user authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import FormComponent from "./Components/FormComponent.js";
+import LogInComponent from "./Components/LogInComponent.js";
 
 const App = () => {
   return (
     <div className="App">
       <FormComponent />
+      <LogInComponent />
     </div>
   );
 }
