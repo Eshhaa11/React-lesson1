e28d1420e457d69620805e061a2d7a0f77fe68b7
Esha Patel <Patellesha11@gmail.com>
2025-03-13 14:44:58 +0300

    feat: Enhance LogInComponent UI with improved layout and styling

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -1,20 +1,22 @@
+import React from 'react';
+import './LogInComponent.css';
+
 const LogInComponent = () => {
     return (
-    <div>
-      <h1>Login</h1>
-    <form>
-  
-      <input type="email" id="email" name="email" required />
-      <label for="email">Email:</label>
-  
-      <input type="password" id="password" name="password" required />
-      <label for="password">Password:</label>
+        <div className="login-container">
+            <h1>Login</h1>
+            <form>
+                <input type="email" placeholder="Email" required />
+                <input type="password" placeholder="Password" required />
+                <button type="submit">Login</button>
+            </form>
 
-  
-      <button type="submit">Login</button>
-    </form>
-    </div>
+            <p className="switch-text">
+                Don't have an account?  
+                <a href="/register" className="link-btn">Register</a>
+            </p>
+        </div>
     );
-  }
-  
-  export default LogInComponent;
+};
+
+export default LogInComponent;
