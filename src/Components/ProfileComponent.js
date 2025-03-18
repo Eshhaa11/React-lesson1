aa70666b9e1e194fd0f57594905b66a1d0611612
Esha Patel <Patellesha11@gmail.com>
2025-03-18 17:45:23 +0300

    feat: Add header element to ProfileComponent for improved structure

--- src/Components/ProfileComponent.js
+++ src/Components/ProfileComponent.js
@@ -14,6 +14,9 @@ const ProfileComponent = () => {
     return (
        <div>
             <h1>Meet The Team</h1>
+            <header>
+                
+            </header>
             </div>
     )
 }
