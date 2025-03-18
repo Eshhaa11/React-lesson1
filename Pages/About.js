45e2886b6c3f687f720ca844af693845a07bca2d
Esha Patel <Patellesha11@gmail.com>
2025-03-18 17:18:05 +0300

    feat: Add ProfileComponent to About page for enhanced team visibility

--- Pages/About.js
+++ Pages/About.js
@@ -1,9 +1,11 @@
 import HeaderComponent from "../src/Components/HeaderComponent";
+import ProfileComponent from "../src/Components/ProfileComponent";
 
 const AboutPage = () => {
     return (
         <div>
             <HeaderComponent/>
+            <ProfileComponent/>
         </div>
     );
 }
