26cc447b38116e99165ea02384275d32d064c482
Esha Patel <Patellesha11@gmail.com>
2025-03-18 17:33:38 +0300

    feat: Create About page component for user information display

---
+++ src/Pages/About.js
@@ -0,0 +1,13 @@
+import HeaderComponent from "../Components/HeaderComponent";
+import ProfileComponent from "../Components/ProfileComponent";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <ProfileComponent/>
+        </div>
+    );
+}
+
+export default AboutPage;
