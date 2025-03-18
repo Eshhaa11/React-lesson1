0c96b5c1f0f7de17048dedf9118343bb1a2c9487
Esha Patel <Patellesha11@gmail.com>
2025-03-18 17:43:26 +0300

    feat: Add team state to ProfileComponent for team member details

--- src/Components/ProfileComponent.js
+++ src/Components/ProfileComponent.js
@@ -1,8 +1,16 @@
 import { useState } from "react";
-import {link} from "react-router-dom"'
-'
+import {link} from "react-router-dom";
+
 
 const ProfileComponent = () => {
+    const [team] = useState ([
+        {
+            name: " Esha Patel",
+            position: "Developer",
+            description: "I'm a passionate developer with a keen eye for detail."
+        }
+    ]
+    })
     return (
        <div>
             <h1>Meet The Team</h1>
