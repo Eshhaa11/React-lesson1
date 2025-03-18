316bb9ce4a9790dbee92e0a02985d4b980edd753
Esha Patel <Patellesha11@gmail.com>
2025-03-18 17:06:29 +0300

    feat: Add AboutPage component with HeaderComponent integration

---
+++ Pages/About.js
@@ -0,0 +1,11 @@
+import HeaderComponent from "../src/Components/HeaderComponent";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+        </div>
+    );
+}
+
+export default AboutPage;
