5ad2c3eae751b190abaac67dfdbe3e8b02c9ee6d
Esha Patel <Patellesha11@gmail.com>
2025-03-19 16:26:41 +0300

    style: Refactor ProfileComponent to use Profile.json for team data configuration

--- src/Components/ProfileComponent.js
+++ src/Components/ProfileComponent.js
@@ -1,15 +1,11 @@
 import { useState } from "react";
-import {link} from "react-router-dom";
+import ProfileData from "../Data/Profile.json"
+
 
 
 const ProfileComponent = () => {
-    const [team] = useState ([
-        {
-            name: " Esha Patel",
-            position: " Lead Developer",
-            description: "I'm a passionate developer with a keen eye for detail."
-        }
-    ])
+    
+    const [team,] = useState ([])
 
     return (
        <div>
