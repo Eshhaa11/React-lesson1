95163453c3c5b2b297337b550eff72096d854ad5
Esha Patel <Patellesha11@gmail.com>
2025-03-19 16:24:22 +0300

    style: Remove unnecessary header element from ProfileComponent for cleaner code

--- src/Components/ProfileComponent.js
+++ src/Components/ProfileComponent.js
@@ -14,9 +14,6 @@ const ProfileComponent = () => {
     return (
        <div>
             <h1>Meet The Team</h1>
-            <header>
-                
-            </header>
             </div>
     )
 }
